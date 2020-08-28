945d855794526616a8e542002e24e93497f78191
Wolfram Webers <wolfram.webers@gmail.com>
2020-08-28 19:29:26 +0200

    Adjusted Z-steps for Titan extruder

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -758,7 +758,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 280.8 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 438 }
 
 /**
  * Default Max Feed Rate (mm/s)
