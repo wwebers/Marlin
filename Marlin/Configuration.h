f39e5258e75194d6add18c47be5542837dfcd7b0
Wolfram Webers <wolfram.webers@gmail.com>
2020-08-29 14:21:01 +0200

    Reverted back to belted extruder

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -760,7 +760,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 438 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 278.9 }
 
 /**
  * Default Max Feed Rate (mm/s)
