8b99765690096e2c2905089342786d1e2d12f36c
Wolfram Webers <wolfram.webers@gmail.com>
2022-04-14 22:05:34 +0200

    Adjusted new probe offset and activated high speed mode for BL-touch

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1183,7 +1183,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -38, 0, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { 40, 0, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
